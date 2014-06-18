eda7d82c658b562839cf9ae35b708a3573b398a7
Avi Kivity <avi@cloudius-systems.com>
2014-06-18 16:50:28 +0300

    java: don't de-symlink openjdk files
    Openjdk has the full path, including the package name and version, embedded
    in RPATH entries within the .so, so we can't just move it to another directory.
    
    Copy the original paths, and add a symlink /usr/lib/jvm/jre so it can be
    accessed from a standard location.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- modules/java/module.py
+++ modules/java/module.py
@@ -1,12 +1,27 @@
 from osv.modules.filemap import FileMap
 from osv.modules import api
+import os, os.path
 
 usr_files = FileMap()
 
 api.require('fonts')
 
-usr_files.add('${jdkbase}').to('/usr/lib/jvm') \
+jdkdir = os.path.basename(os.path.expandvars('${jdkbase}'))
+if not os.path.exists('diskimage/usr/lib/jvm'):
+    os.makedirs('diskimage/usr/lib/jvm')
+
+def symlink_force(dest, name):
+    if os.path.lexists(name):
+        os.unlink(name)
+    os.symlink(dest, name)
+
+symlink_force(jdkdir, 'diskimage/usr/lib/jvm/java')
+symlink_force(jdkdir + '/jre', 'diskimage/usr/lib/jvm/jre')
+
+usr_files.add('${jdkbase}').to('/usr/lib/jvm/' + jdkdir) \
     .include('lib/**') \
     .include('jre/**') \
     .exclude('jre/lib/security/cacerts') \
     .exclude('jre/lib/audio/**')
+usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/java').to('/usr/lib/jvm/java')
+usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/jre').to('/usr/lib/jvm/jre')
